d1c95929c95f1859dfd9fddf351eb1387e36c780
Noah Stevens <stevensnas12@gmail.com>
2024-05-03 17:56:37 -0400

    added elements for 3 possible hints

--- front-end/src/components/HintBox.vue
+++ front-end/src/components/HintBox.vue
@@ -1,12 +1,15 @@
 <template>
-
+    <div id="hint-box">
+        <p v-if="guessNumber >= 2">Continent: {{ this.$store.state.flag.continent }}</p>
+        <p v-if="guessNumber >= 3">Capital: {{ this.$store.state.flag.capital }}</p>
+        <p v-if="guessNumber >=4">Country Code: {{ this.$store.state.flag.code }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    props: ["guessNumber"]
 }
-
 </script>
 
 <style>
