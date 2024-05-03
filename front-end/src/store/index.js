665db4307b26a2c2b49657d86618e90bd9bdf2d0
Noah Stevens <stevensnas12@gmail.com>
2024-05-03 17:48:51 -0400

    Added array of country names and mutator for populating the array

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -9,13 +9,17 @@ export function createStore() {
                 capital: "",
                 continent: "",
                 flagImage: ""
-            }
+            },
+            countryNames: []
         },
 
         mutations: {
             SET_FLAG(state, payload) {
                 state.flag = payload;
             },
+            POPULATE_LIST(state, payload) {
+                state.countryNames = payload;
+            }
         }
     });
 }
