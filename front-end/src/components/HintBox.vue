2bfac39bb83191d833e357039712d2b5f38d522f
Noah Stevens <stevensnas12@gmail.com>
2024-05-03 21:12:19 -0400

    styling and reformat hints into spans

--- front-end/src/components/HintBox.vue
+++ front-end/src/components/HintBox.vue
@@ -1,8 +1,18 @@
 <template>
-    <div id="hint-box">
-        <p v-if="guessNumber >= 2">Continent: {{ this.$store.state.flag.continent }}</p>
-        <p v-if="guessNumber >= 3">Capital: {{ this.$store.state.flag.capital }}</p>
-        <p v-if="guessNumber >=4">Country Code: {{ this.$store.state.flag.code }}</p>
+    <div id="hint-box" v-if="guessNumber >= 2">
+        <h2>HINTS:</h2>
+        <span id="continent-span" v-if="guessNumber >= 2">
+            <p id="continent">Continent:</p>
+            <p id="continent-name">{{ this.$store.state.flag.continent }}</p>
+        </span>
+        <span id="capital-span" v-if="guessNumber >= 3">
+            <p>Capital:</p> 
+            <p>{{ this.$store.state.flag.capital }}</p>
+        </span>
+        <span id="code-span" v-if="guessNumber >=4">
+            <p>Country Code:</p>
+            <p>{{ this.$store.state.flag.code }}</p>
+        </span>
     </div>
 </template>
 
@@ -13,5 +23,51 @@ export default {
 </script>
 
 <style>
+#hint-box {
+    background-color: #455d79;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-areas: 
+    "title"
+    "hint1"
+    "hint2"
+    "hint3";
+    border-style: solid;
+    border-width: 3px;
+    border-color: black;
+    border-radius: 15px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 50px;
+    padding-right: 50px;
+    margin-top: 50px;
+}
 
+#hint-box>h2 {
+    display: flex;
+    grid-area: title;
+    justify-self: center;
+}
+
+#continent-span {
+    grid-area: hint1;
+    display: flex;
+    justify-content: space-between;
+}
+
+#capital-span {
+    grid-area: hint2;
+    display: flex;
+    justify-content: space-between;
+}
+
+#code-span {
+    grid-area: hint3;
+    display: flex;
+    justify-content: space-between;
+}
+
+#hint-box p {
+    font-size: x-large;
+}
 </style>
