0e97327d8b1006a1190d976f89e0c1e736bfa33d
Noah Stevens <stevensnas12@gmail.com>
2024-05-05 02:26:21 -0400

    added stats object that gets saved to browser local storage

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -10,7 +10,21 @@ export function createStore() {
                 continent: "",
                 flagImage: ""
             },
-            countryNames: []
+            countryNames: [],
+            stats: {
+                gamesPlayed: 0,
+                gamesWon: 0,
+                guesses: {
+                    1: 0,
+                    2: 0,
+                    3: 0,
+                    4: 0,
+                    fail: 0
+                },
+                currentStreak: 0,
+                maxStreak: 0,
+                hasPlayed: false
+            },
         },
 
         mutations: {
@@ -19,7 +33,18 @@ export function createStore() {
             },
             POPULATE_LIST(state, payload) {
                 state.countryNames = payload;
-            }
+            },
+            SYNC_STATS(state) {
+                if (localStorage.getItem("stats") != null) {
+                    state.stats = JSON.parse(localStorage.getItem("stats"));
+                } else {
+                    localStorage.setItem("stats", JSON.stringify(state.stats));
+                }
+            },
+            UPDATE_STATS(state, payload) {
+                state.stats = payload;
+                localStorage.setItem("stats", JSON.stringify(state.stats));
+            },
         }
     });
 }
