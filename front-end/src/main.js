065fe9485bfc1ce9748d194026db85f4c6258c51
Noah Stevens <stevens_nas@yahoo.com>
2024-06-04 18:43:48 -0400

    Updated imports for Netlify compatability

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
 
-import { createApp } from './vue'
-import { createStore } from './store'
+import { createApp } from '../node_modules/vue'
+import { createStore } from '../node_modules/vuex'
 
 import App from './App.vue'
-import router from './router'
-import axios from './axios'
+import router from '../node_modules/vue-router'
+import axios from '../node_modules/axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API;
 
