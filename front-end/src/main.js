7786607fcb4574e67b1123c05eced979a5170d02
Noah Stevens <stevens_nas@yahoo.com>
2024-06-04 18:27:00 -0400

    axios base URL now pull from env variable

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,8 +5,13 @@ import { createStore } from './store'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-const store = createStore();
+axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API;
+
+let currentStats = JSON.parse(localStorage.getItem("stats"));
+
+const store = createStore(currentStats);
 const app = createApp(App)
 
 app.use(store);
