819c110c4499e2966e64c21956db66dd2f31495b
Noah Stevens <stevens_nas@yahoo.com>
2024-06-04 18:31:16 -0400

    Now uses preconfigured axios object

--- front-end/src/services/FlagService.js
+++ front-end/src/services/FlagService.js
@@ -1,20 +1,16 @@
 import axios from "axios";
 
-const http = axios.create({
-    baseURL: "https://flag-guesser-server.onrender.com"
-});
-
 export default {
 
     getDaily() {
-        return http.get("/daily");
+        return axios.get("/daily");
     },
 
     getPractice() {
-        return http.get("/practice");
+        return axios.get("/practice");
     },
 
     getList() {
-        return http.get("/list");
+        return axios.get("/list");
     }
 }
