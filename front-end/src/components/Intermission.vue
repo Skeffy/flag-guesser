1744f678a1cd8c80a39bae4486695388d862f4bd
Noah Stevens <stevens_nas@yahoo.com>
2024-10-08 15:35:54 -0400

    Initial commit of practice mode intermission screen

---
+++ front-end/src/components/Intermission.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="intermission">
+        <div id="correct" v-if="isCorrect">
+            <h1>CORRECT!</h1>
+        </div>
+        <div id="incorrect" v-if="!isCorrect">
+            <h1>SORRY!</h1>
+        </div>
+        <button @click="nextFlag">Next Flag</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["isCorrect"],
+
+    methods: {
+        nextFlag() {
+            this.$emit('nextFlag', false);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
