982aa593a831dca29c429e3f2271e973629e7d38
Noah Stevens <stevens_nas@yahoo.com>
2024-10-11 22:52:29 -0400

    Now controlls retrieving next flag after "next flag" is clicked. Styling changes

--- front-end/src/components/Intermission.vue
+++ front-end/src/components/Intermission.vue
@@ -1,27 +1,51 @@
 <template>
     <div id="intermission">
+        <div id="flag">
+            <img :src="this.$store.state.flag.flagImage" alt="">
+        </div>
         <div id="correct" v-if="isCorrect">
-            <h1>CORRECT!</h1>
+            <h2>{{ this.$store.state.flag.name }} is correct!</h2>
         </div>
         <div id="incorrect" v-if="!isCorrect">
-            <h1>SORRY!</h1>
+            <h2>Sorry! The correct answer was {{ this.$store.state.flag.name }}</h2>
         </div>
-        <button @click="nextFlag">Next Flag</button>
+        <button @click="nextFlag" class="gamebtn">Next Flag</button>
     </div>
 </template>
 
 <script>
+import FlagService from '../services/FlagService.js';
+
 export default {
     props: ["isCorrect"],
 
     methods: {
         nextFlag() {
+            FlagService.getPractice().then( (response) => {
+                    this.$store.commit("SET_FLAG", response.data);
+                });
             this.$emit('nextFlag', false);
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+#intermission {
+    display: grid;
+    justify-content: center;
+}
 
+#flag {
+    padding-bottom: 40px;
+}
+
+.gamebtn {
+    margin-top: 10px;
+    justify-self: center;
+}
+
+#correct, #incorrect {
+    justify-self: center;
+}
 </style>
