3b8bceedc3daef7587ab18b37ce8666fce4b565f
Noah Stevens <stevens_nas@yahoo.com>
2024-10-18 00:57:32 -0400

    Initial commit

---
+++ front-end/src/components/StatsChart.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="stat-chart">
+        <Bar
+            id="player-stats-chart"
+            :options="chartOptions"
+            :data="playerStats"
+        />
+    </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      playerStats: {
+        labels: [ '1', '2', '3', '4', 'Fail' ],
+        datasets: [ { 
+            label: 'Number of Guesses',
+            data: this.$store.state.stats.guesses,
+            backgroundColor: '#d7263d',
+            color: 'black' } ]
+      },
+      chartOptions: {
+        responsive: true,
+        animation: true,
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#stat-chart {
+    width: 80vw;
+    margin-left: 50px;
+    margin-right: 50px;
+    margin-top: 50px;
+    background-color: #41446c;
+}
+</style>
