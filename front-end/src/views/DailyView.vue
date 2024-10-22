7da2642ca5c19beffae6ae0c4ba826957a424dd0
Noah Stevens <stevens_nas@yahoo.com>
2024-10-21 21:29:32 -0400

    listener for updateStats has changed to gameOver. Logic added to reset currentStreak to 0 after losing a game

--- front-end/src/views/DailyView.vue
+++ front-end/src/views/DailyView.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="daily-view">
         <pageHeader :gameMode="gameMode" />
-        <gameContainer @updateStats="updateStats" :gameMode="gameMode" :flag="this.$store.state.daily" v-if="isLoaded"/>
+        <gameContainer @gameOver="gameOver" :gameMode="gameMode" :flag="this.$store.state.daily" v-if="isLoaded"/>
         <div v-if="this.$store.state.stats.hasPlayed" id="completed">
             <h2>You've already completed todays challenge.</h2>
             <h2>Today's flag was {{ this.$store.state.daily.name }}</h2>
@@ -39,25 +39,25 @@ export default {
     },
 
     methods: {
-        updateStats() {
+        gameOver(hasWon, guessNumber) {
             var stats = this.$store.state.stats;
             stats.gamesPlayed++;
-            if (this.hasWon) {
+            stats.hasPlayed = true;
+            if (hasWon) {
                 stats.gamesWon++;
                 stats.currentStreak++;
                 if (stats.currentStreak > stats.maxStreak) {
                     stats.maxStreak = stats.currentStreak;
                 }
                 for (var key in stats.guesses) {
-                    if (key == this.guessNumber) {
+                    if (key == guessNumber) {
                         stats.guesses[key]++;
                     }
                 }
             } else {
                 stats.guesses.fail++;
+                stats.currentStreak = 0;
             }
-            stats.currentGuessNumber = 1;
-            stats.hasPlayed = true;
             stats.timestamp = Math.floor(Date.now() / 1000);
             this.$store.commit("UPDATE_STATS", stats);
         },
