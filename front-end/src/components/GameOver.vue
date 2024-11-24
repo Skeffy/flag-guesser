36bcbf36fb7faa91aab10a18ca393d52d5d3b4c6
Noah Stevens <stevens_nas@yahoo.com>
2024-11-24 13:08:29 -0500

    remove unused component

--- front-end/src/components/GameOver.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <b-modal id="game-over" title="BootstrapVue">
-        <p class="my-4">Hello from modal!</p>
-    </b-modal>
-</template>
-  
-<script>    
-import { BModal } from "bootstrap-vue";
-</script>
