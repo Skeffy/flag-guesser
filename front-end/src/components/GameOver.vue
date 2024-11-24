a21b6e8721ddb2641d510217772325c90021d5ed
Noah Stevens <stevens_nas@yahoo.com>
2024-11-24 12:27:45 -0500

    initial commit

---
+++ front-end/src/components/GameOver.vue
@@ -0,0 +1,9 @@
+<template>
+    <b-modal id="game-over" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+    </b-modal>
+</template>
+  
+<script>    
+import { BModal } from "bootstrap-vue";
+</script>
