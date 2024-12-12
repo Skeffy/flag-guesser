2d8efac58e62c9de4b3bf952d046d03a2e140ae2
Noah Stevens <stevens_nas@yahoo.com>
2024-12-11 20:04:22 -0500

    Added stat containers for each tracker in store

--- front-end/src/views/StatsView.vue
+++ front-end/src/views/StatsView.vue
@@ -1,17 +1,35 @@
 <template>
-    <StatsChart />
+    <div id="stats">
+        <div id="trackers">
+            <StatContainer v-for="stat in this.$store.state.stats.trackers" :stat="stat" />
+        </div>
+        <StatsChart />
+    </div>
 </template>
 
 <script>
 import StatsChart from '@/components/StatsChart.vue';
+import StatContainer from '@/components/StatCointainer.vue';
 
 export default {
     components: {
-        StatsChart
+        StatsChart,
+        StatContainer,
     },
 }
 </script>
 
 <style>
+    #stats {
+        display: flex;
+        flex-direction: column;
+    }
 
+    #trackers {
+        display: flex;
+        justify-content: space-between;
+        width: 90%;
+        margin-left: auto;
+        margin-right: auto;
+    }
 </style>
