2d7540eff4e54662823bde79ec381720d0c89a2e
Noah Stevens <stevens_nas@yahoo.com>
2024-12-11 20:04:51 -0500

    Initial commit

---
+++ front-end/src/components/StatCointainer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="stat">
+        <h3>{{ this.stat.name }}</h3>
+        <h3>{{ this.stat.value }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["stat"],
+    }
+</script>
+
+<style>
+    .stat {
+        display: flex;
+        flex-direction: column;
+        width: 200px;
+        height: 100px;
+        color: black;
+        background-color: #f3f4f7;
+        justify-content: center;
+        border-radius: 10px;
+        border: 3px solid #d7263d;
+    }
+</style>
